0d894f5988a702fe2db5b398ead3003d724cef5e
lance <lancetj139@outlook.com>
2025-11-24 11:20:39 +0800

    Utils:  Enhance the tolerance for differences in value types

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,16 +1,19 @@
 import configparser
+import ast  # Import for safe literal evaluation
 
 # Function to load configuration from ini file
 def load_config(config_path='config.ini'):
     config = configparser.ConfigParser()
     config.read(config_path)
+    allowed_feature_names_str = config['pss']['allowed_feature_names']
+    allowed_feature_names = [ft.strip() for ft in allowed_feature_names_str.split(',')]
     return {
         'timeout': float(config['pss']['timeout_sec']),
         'short_threshold': int(config['pss']['short_threshold']),
         'long_threshold': int(config['pss']['long_threshold']),
         'batch_size': int(config['pss']['batch_size']),
-        'lambda_dict': eval(config['pss']['lambda_dict']),
-        'feature_weights': eval(config['pss']['feature_weights']),
+        'lambda_dict': ast.literal_eval(config['pss']['lambda_dict']),
+        'feature_weights': ast.literal_eval(config['pss']['feature_weights']),
         'regularization_lambda': float(config['pss']['regularization_lambda']),
         'max_workers_cfm': int(config['cfm']['max_workers']),
         'timeout_cfm': int(config['cfm']['timeout_min']),
@@ -18,5 +21,6 @@ def load_config(config_path='config.ini'):
         'mask_features_concat': [f.strip() for f in config['concat']['mask_features'].split(',')],
         'max_workers_label': int(config['label']['max_workers']),
         'mask_features_label': [f.strip() for f in config['label']['mask_features'].split(',')],
-        'rules_file_label': config['label']['rules_file']
+        'rules_file_label': config['label']['rules_file'],
+        'allowed_feature_names': allowed_feature_names  # Add list conversion for allowed_feature_names
     }
